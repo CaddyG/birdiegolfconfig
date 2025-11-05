89f919949c486d92768548752b295145802c8f3f
CaddyG <spottedowl4040@gmail.com>
2025-11-04 22:32:50 -0500

    Autocommit from 2025-11-04 22:32:50
    Klipper version: v0.13.0-335-gb85b92fd
    
    Moonraker version: v0.9.3-121-gf2bb93f
    
    Fluidd version: v1.34.4

--- autocommit.sh
+++ autocommit.sh
@@ -18,17 +18,17 @@ config_folder=~/printer_data/config
 # provided moonraker script 'data-path-fix.sh' to fix/update
 # older installs
 
-### Path to your Klipper folder, by default that is '~/klipper'
+###Path to your Klipper folder, by default that is '~/klipper'
 klipper_folder=~/klipper
 
-### Path to your Moonraker folder, by default that is '~/moonraker'
+###Path to your Moonraker folder, by default that is '~/moonraker'
 moonraker_folder=~/moonraker
 
 ### Path to your Mainsail folder, by default that is '~/mainsail'
-mainsail_folder=~/mainsail
+#mainsail_folder=~/mainsail
 
-### Path to your Fluidd folder, by default that is '~/fluidd'
-#fluidd_folder=~/fluidd
+###Path to your Fluidd folder, by default that is '~/fluidd'
+fluidd_folder=~/fluidd
 
 ### The branch of the repository that you want to save your config
 ### By default that is 'main'
